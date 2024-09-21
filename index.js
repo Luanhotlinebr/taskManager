8265e5a8d4050afba75bcbf677c5578598244c71
Luanhotlinebr <luanhotlinebr60@gmail.com>
2024-09-21 11:20:53 -0300

    feat: connect to mongodb database

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-//Instalar Framework xpress
-//Express é um servidor
-//npm i express
-
-//npm run start:dev > Posso executar o script que escrevi no package.json para testar
 
 const express = require('express');
+const dotenv = require("dotenv");
+
+const connectToDatabase = require('./src/database/mongoose.database')
+dotenv.config();
 
 const app = express();
+
+connectToDatabase();
+
 //rota
-app.get("/",(req,res)=>{
-    res.status(200).send('Hello World!');
-})
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World!");
+});
 //Iniciou o back end
 app.listen(8000, () => console.log("Listening on port 8000!"));
---
+++ src/database/mongoose.database.js
@@ -0,0 +1,13 @@
+//Criei uma constante para importar o mongoose que se comunica com o Mongo DB.
+const mongoose = require("mongoose");
+
+//Criei uma função assicrona que conecta o mongoose com o MongoDB.
+const connectToDatabase = async () => {
+    await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.hywzt.mongodb.net/?retryWrites=true&w=majority&appName=FscTaskManagerCluster`
+).then(()=> console.log('Connected to MongoDB!'));
+};
+
+//Criei um modo de exportar a função connectToDatabase para o arquivo principal.
+module.exports = connectToDatabase;
+
