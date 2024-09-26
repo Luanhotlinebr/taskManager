dd6d3d1e0b46481118f746e47538acb6c214bb06
Luanhotlinebr <luanhotlinebr60@gmail.com>
2024-09-26 19:28:32 -0300

    refactor: converts components to class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import { useState } from "react";
-import TaskItem from "./components/TaskItem";
-//Sempre que um compoentente renderizar outro compoentne, o que rederizou será o pai e o renderizado é o filho.
-const App = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            description: 'Estudar Programação',
-            isCompleted: false,
-        },
-        {
-            id: 2,
-            description: 'Ler',
-            isCompleted: true,
+import React from 'react'
+import TaskItem from "./components/TaskItem"
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleStatChange = this.handleStatChange.bind(this);
+        this.state = {
+            tasks: [
+                {
+                    id: "1",
+                    description: "Estudar Programação",
+                    isCompleted: false,
+                },
+                {
+                    id: "2",
+                    description: "Ler",
+                    isCompleted: true,
+                },
+            ]
         }
-    ]);
+    }
 
-
-    return (
-        <>
-            {tasks.map((task) => (<TaskItem key={task.id} task={task}/>)) }
-        </>
-    )
-};
+   //Método da classe:
+   handleStatChange (){
+    this.setState({
+        tasks: [],
+    })
+   }     
+    render () {
+        return (
+            <>
+                {this.state.tasks.map((task)=>(
+                    <TaskItem key={task.id} task={task}/>
+                ))}
+                <button onClick={this.handleStatChange}>Limpar tarefas</button>
+            </>
+        )
+    }
+}
 
 export default App;
+
+// import { useState } from "react";
+// import TaskItem from "./components/TaskItem";
+// //Sempre que um compoentente renderizar outro compoentne, o que rederizou será o pai e o renderizado é o filho.
+// const App = () => {
+//     const [tasks, setTasks] = useState([
+//         {
+//             id: 1,
+//     ]);
+//             description: 'Estudar Programação',
+//             isCompleted: false,
+//         },
+//         {
+//             id: 2,
+//             description: 'Ler',
+//             isCompleted: true,
+//         }
+
+
+//     return (
+//         <>
+//             {tasks.map((task) => (<TaskItem key={task.id} task={task}/>)) }
+//         </>
+//     )
+// };
+
+// export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,13 +1,29 @@
-//Podemos utilizar assim: const TaskItem = (props) =>
-//ou:
-//Assim: const TaskItem = ({task}) =>
-const TaskItem = (props) => {
-    return (
-        <>
-            <h1>{props.task.description}</h1>
-            <p>{props.task.isCompleted ? 'Completa' : 'Não completa'}</p>
-        </>
-    )
+
+// const TaskItem = (props) => {
+//     return (
+//         <>
+//             <h1>{props.task.description}</h1>
+//             <p>{props.task.isCompleted ? 'Completa' : 'Não completa'}</p>
+//         </>
+//     )
+// }
+
+// export default TaskItem;
+
+import React from 'react';
+
+class TaskItem extends React.Component {
+    render()
+    {
+        const {task} = this.props;
+
+        return (
+            <>
+            <h1>{this.props.task.description}</h1>
+            <p >{task.isCompleted ? "Completa" : "Não completa"} </p>
+            </>
+        )
+    }
 }
 
 export default TaskItem;
