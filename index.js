4f44697a0e93bd98bfcd3b718face50c6fa786aa
Luanhotlinebr <luanhotlinebr60@gmail.com>
2024-09-27 19:13:51 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const TaskRouter = require("./src/routes/task.routes");
+const cors = require('cors');
 
+const TaskRouter = require("./src/routes/task.routes");
 const connectToDatabase = require("./src/database/mongoose.database");
 
 dotenv.config();
 const app = express();
+app.use(cors());
 //MiddleWare - Receber Json na Body das Requisições
 app.use(express.json());
 
