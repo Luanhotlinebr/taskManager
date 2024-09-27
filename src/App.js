fc86d72858eb3d5a1b7d5179d2b5259e45815b8b
Luanhotlinebr <luanhotlinebr60@gmail.com>
2024-09-27 19:28:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,68 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import TaskItem from "./components/TaskItem"
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleStatChange = this.handleStatChange.bind(this);
-        this.state = {
-            tasks: [
-                {
-                    id: "1",
-                    description: "Estudar Programação",
-                    isCompleted: false,
-                },
-                {
-                    id: "2",
-                    description: "Ler",
-                    isCompleted: true,
-                },
-            ]
+
+const App = () => {
+    const [tasks, setTasks] = useState ([
+        {
+            id: "1",
+            descripton: "Estudar Programação",
+            isCompleted: false,
+        },
+        {
+            id: "2",
+            descripton: "Ler",
+            isCompleted: true,
+        }
+    ]);
+
+    const fetchTasks = async () => {
+        try {
+            const {data} = await axios.get("http://localhost:8000/tasks");
+            
+            setTasks(data);
+        }catch (error){
+            console.log(error);
         }
     }
 
-   //Método da classe:
-   handleStatChange (){
-    this.setState({
-        tasks: [],
-    })
-   }     
-    render () {
-        return (
-            <>
-                {this.state.tasks.map((task)=>(
-                    <TaskItem key={task.id} task={task}/>
-                ))}
-                <button onClick={this.handleStatChange}>Limpar tarefas</button>
-            </>
-        )
-    }
-}
+    useEffect(() =>{
+        fetchTasks();
+    },[]);
+
+    return(<>
+        {tasks.map((task)=>(
+            <TaskItem key={task.id} task={task}/>
+        ))}
+    </>)
+};
 
 export default App;
-
-// import { useState } from "react";
-// import TaskItem from "./components/TaskItem";
-// //Sempre que um compoentente renderizar outro compoentne, o que rederizou será o pai e o renderizado é o filho.
-// const App = () => {
-//     const [tasks, setTasks] = useState([
-//         {
-//             id: 1,
-//     ]);
-//             description: 'Estudar Programação',
-//             isCompleted: false,
-//         },
-//         {
-//             id: 2,
-//             description: 'Ler',
-//             isCompleted: true,
-//         }
-
-
-//     return (
-//         <>
-//             {tasks.map((task) => (<TaskItem key={task.id} task={task}/>)) }
-//         </>
-//     )
-// };
-
-// export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,29 +1,9 @@
-
-// const TaskItem = (props) => {
-//     return (
-//         <>
-//             <h1>{props.task.description}</h1>
-//             <p>{props.task.isCompleted ? 'Completa' : 'Não completa'}</p>
-//         </>
-//     )
-// }
-
-// export default TaskItem;
-
-import React from 'react';
-
-class TaskItem extends React.Component {
-    render()
-    {
-        const {task} = this.props;
-
-        return (
-            <>
-            <h1>{this.props.task.description}</h1>
+const TaskItem = ({ task }) => {
+    return (
+        <>
+            <h1>{task.description}</h1>
             <p >{task.isCompleted ? "Completa" : "Não completa"} </p>
-            </>
-        )
-    }
-}
-
+        </>
+    );
+};
 export default TaskItem;
