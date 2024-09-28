bf61417e83d06f13413636b718ca7867fa23a452
Luanhotlinebr <luanhotlinebr60@gmail.com>
2024-09-28 11:35:41 -0300

    feat: add react-alert-setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+    positions,
+    transitions,
+    Provider as AlertProvider
+} from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
-import './index.scss';
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const options = {
+    position: positions.BOTTOM_CENTER,
+    timeout: 2600,
+    offset: "30px",
+    transition: transitions.SCALE,
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <AlertProvider template={AlertTemplate} {...options}>
+            <App />
+        </AlertProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
